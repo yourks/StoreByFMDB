f277c176f7cc3e184e9daab000af227c4dd11dd5
yourks <568986318@qq.com>
2017-04-07 14:47:18 +0800

    初始化项目

---
+++ YKStroreFMDB/YKStroreFMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YKStroreFMDB
+//
+//  Created by Apple on 17/4/7.
+//  Copyright © 2017年 YoursKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YKStroreFMDB/YKStroreFMDB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YKStroreFMDB
+//
+//  Created by Apple on 17/4/7.
+//  Copyright © 2017年 YoursKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
